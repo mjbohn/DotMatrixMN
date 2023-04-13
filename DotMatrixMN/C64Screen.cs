3060e1efb2f531f108d6b11b11137c48c5ff5c32
mjbohn <mjbohn@gmail.com>
2023-04-13 15:22:50 +0200

    refactoring

--- DotMatrixMN/C64Screen.cs
+++ DotMatrixMN/C64Screen.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace DotMatrixMN
@@ -11,16 +6,16 @@ namespace DotMatrixMN
     public class C64Screen : UserControl
     {
         List<Matrix[]> screen = new List<Matrix[]>();
-        int _border;  
+        int _border;
 
         public C64Screen() : base()
         {
             SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
-            
+
             BackColor = Color.FromArgb(0x80, 0xff, 0xff);
             _border = 15;
             SetupScreen(_border);
-            
+
         }
 
         // avoid rezising of control in designer
@@ -29,7 +24,7 @@ namespace DotMatrixMN
             base.SetBoundsCore(x, y, 640 + (2 * _border), 400 + (2 * _border), specified);
         }
 
-        private void SetupScreen() { SetupScreen(0);}
+        private void SetupScreen() { SetupScreen(0); }
         private void SetupScreen(int border)
         {
             screen.Clear();
@@ -43,19 +38,17 @@ namespace DotMatrixMN
                 for (int j = 0; j < 40; j++)
                 {
                     Matrix matrix = new Matrix();
-                    //Matrix matrix = new Matrix(8,8,new Size(25,25));
+                    
                     matrix.ScreenRow = i;
                     matrix.ScreenColumn = j;
                     matrix.Location = new Point(j * matrix.Width + border, i * matrix.Height + border);
-                    
-                    //if (j % 2 == 0) { matrix.ColorDisabled = Color.Red; }
 
                     row[j] = matrix;
-                    this.Controls.Add(matrix);
+                    Controls.Add(matrix);
                 }
-                this.SuspendLayout();
+
                 screen.Add(row);
-                this.ResumeLayout();
+
             }
 
         }
